57017ffbb76b336c70c0aa21a69520cae15b9b33
DeathbatO13 <danielalejo99@gmail.com>
2025-09-23 14:51:28 -0500

    Corregir Win

---
+++ src/motor/Posicion.java
@@ -0,0 +1,47 @@
+package motor;
+
+/**
+ * Representa una posición dada por dos corrdenadas: fila y columna.
+ * @author DeathbatO
+ */
+public class Posicion {
+    private byte fila, columna;
+    
+    /**
+     * Crea una nueva posición con los valores pasados como argumentos.
+     * @param fila La fila de la posición o coordenada.
+     * @param columna La columna de la posicíon o coordenada.
+     */
+    public Posicion(byte fila, byte columna){
+    	this.fila = fila;
+    	this.columna = columna;
+    }
+
+    /**
+     * Retorna la fila de coordenada.
+     * @return La fila.
+     */
+    public byte getFila(){
+    	return this.fila;
+    }
+
+    /**
+     * Asigna la fila de la coordenada. Si esta es menor que 0 se establece en cero. 
+     * @param fila La fila de la coordenada.
+     */
+    public void setFila(byte fila){
+    	this.fila = fila;
+    }
+
+    /**
+     * La columna de la coordenada o posición.
+     * @return La columna de la posición.
+     */
+    public byte getColumna(){
+    	return this.columna;
+    }
+
+    public void setColumna(byte columna){
+    	this.columna = columna;
+    }
+}
---
+++ src/motor/Tablero.java
@@ -0,0 +1,145 @@
+package motor;
+
+import motor.Celda.*;
+
+/**
+ * Representa un tablero del juego Buscaminas como una grilla de objetos de tipo
+ * {@link Celda}. Se definen además las constantes que se usarán para definir los
+ * distintos estados del juego.
+ * @author DeathbatO
+ */
+public class Tablero {
+    //Valores predefinidos para las dimensiones maximas y minimas del tablero
+    public static byte
+        MIN_ANCHO=5,
+        MIN_LARGO=5,
+        MAX_ANCHO=100,
+        MAX_LARGO=100; 
+
+    //El tablero: una grilla con topes.
+    private final Celda[][] celdas;
+    private int cantidadMinas;
+    private byte cantFilas, cantColumnas;
+    
+    /**
+     * Crea un nuevo tablero with las dimensiones indicadas. Las celdas del mismo
+     * estarán todas OCULTAS y sin minas. Las cantidades de filas y columnas deben
+     * ser mayores que MIN_ANCHO y MIN_LARGO respectivamente, de lo contrario se
+     * establecerán con dichos valores.
+     * @param cantidadFilas La cantidad de filas que contendrá el tablero.
+     * @param cantidadColumnas La cantidad de columnas que contendrá el tablero.
+     */
+    public Tablero(byte cantidadFilas, byte cantidadColumnas){
+    	
+        if(cantidadFilas <= MIN_ANCHO){
+        	this.cantFilas = MIN_ANCHO;
+        }
+        if(cantidadColumnas <= MIN_LARGO){
+        	this.cantColumnas = MIN_LARGO;
+        }
+        if(cantidadFilas >= MAX_ANCHO){
+        	this.cantFilas = MAX_ANCHO;
+        }
+        if(cantidadColumnas >= MAX_LARGO){
+        	this.cantColumnas = MAX_LARGO;
+        }
+        
+        this.cantFilas = cantidadFilas;
+        this.cantColumnas = cantidadColumnas;
+        this.celdas = new Celda[cantidadFilas][cantidadColumnas];
+
+        // Inicializar todas las celdas como ocultas y sin minas
+        for (int i = 0; i < this.cantFilas; i++) {
+            for (int j = 0; j < this.cantColumnas; j++) {
+                this.celdas[i][j] = new Celda(false, EstadoCelda.OCULTA, (byte)0);
+            }
+        }
+        
+        this.cantidadMinas = 0;
+    }
+    
+    /**
+     * Indica el largo (cantidad de columnas) del tablero.
+     * @return La cantidad de columnas en el tablero.
+     */
+    public byte getLargo(){
+        return this.cantColumnas;
+    }
+    
+    /**
+     * Indica el ancho (cantidad de filas) del tablero.
+     * @return La cantidad de filas del tablero.
+     */
+    public byte getAncho(){
+        return this.cantFilas;
+    }
+    
+    /**
+     * Indica si una posición es válida para el tablero.
+     * @param fila La posición de la fila (la primera fila es 0).
+     * @param columna La posición de la columna (la primera columna es 0).
+     * @return TRUE si la posición es adecuada en el tablero, FALSE si no lo es.
+     */
+    public boolean esPosicionValida(byte fila, byte columna){
+         if((fila <= this.cantFilas-1 && fila >= 0) && (columna <= this.cantColumnas-1 && columna >= 0)){
+        	 return true;
+         }
+         return false;
+    }
+    
+    /**
+     * Si se pasa TRUE en tieneBomba se establecerá la celda actual como una celda
+     * con mina (salvo que ésta ya estuviera marcada así); en tal caso se sumará
+     * 1 bomba circundante a todas las celdas alrededor de la posición dada. Si se
+     * pasa FALSE en tieneBomba se establecerá la celda actual como una celda sin
+     * mina (salvo que ya estuviera marcada así); en tal caso se restará 1 bomba
+     * circundante a todas las celdas alrededor de la posición dada.<br><br>
+     * <b>PRECONDICIÓN:</b> La posicíon dada {@link esPosicionValida}.
+     * @param tieneBomba TRUE para indicar que la celda contiene bomba, FALSE si no.
+     * @param fila La fila en que está la celda.
+     * @param columna La columna en que está la celda.
+     */
+    public void asignarBomba(boolean tieneBomba, byte fila, byte columna){
+        if(esPosicionValida(fila, columna)){
+            Celda celda = this.celdas[fila][columna];
+            if(tieneBomba) {
+                if(!celda.tieneBomba()) {
+                    celda.setTieneBomba(true);
+                }
+                // If already has bomb, do nothing
+            } else {
+                if(celda.tieneBomba()) {
+                    celda.setTieneBomba(false);
+                }
+                // If already does not have bomb, do nothing
+            }
+        }
+    }
+    
+    /**
+     * Retorna la celda en la posición dada.<br><br>
+     * <b>PRECONDICIÓN</b>: La posición {@link esPosicionValida}.
+     * @param fila La fila de la celda buscada.
+     * @param columna La columna de la celda buscada.
+     * @return La celda en la posición dada.
+     */
+    public Celda getCelda(byte fila, byte columna){
+        return this.celdas[fila][columna];
+    }
+    
+    /**
+     * Indica la cantidad total de celdas existentes en el tablero.
+     * @return La cantidad de celdas en el tablero.
+     */
+    public int getCantidadCeldas(){
+        return (int)(this.cantColumnas * this.cantColumnas);
+    }
+    
+    /**
+     * Indica la cantidad de minas en el tablero.
+     * @return La cantidad de minas en el tablero.
+     */
+    public int getCantidadMinas(){
+        return this.cantidadMinas;
+    }
+}
